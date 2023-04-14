5052df3f0ef6406f9e7e46c9aef7f57396946d0b
thewmking <thewmking@gmail.com>
2023-04-14 11:28:03 -0400

    Split up logic into methods

--- app/main.rb
+++ app/main.rb
@@ -2,36 +2,44 @@ require 'app/sprites/sprite.rb'
 require 'app/sprites/frog_1.rb'
 
 def tick args
+  init(args)
+  handle_input(args)
+  enforce_boundaries(args)  
+  render(args)
+end
 
-  # init
+def render(args)
+  args.outputs.sprites << [args.state.player_x, args.state.player_y, 100, 80, 'sprites/misc/dragon-0.png']
+end
+
+def init(args)
   args.state.player_x ||= 120
   args.state.player_y ||= 280
-  speed = diagonal?(args) ? 6 : 12
-  player_w = 100
-  player_h = 80
-
-  # controls
-  args.state.player_x += args.inputs.left_right * speed
-  args.state.player_y += args.inputs.up_down * speed
-
-  if args.state.player_x + player_w > args.grid.w
-    args.state.player_x = args.grid.w - player_w
-  end
-
-  if args.state.player_y + player_h > args.grid.h
-    args.state.player_y = args.grid.h - player_h
-  end
-
-  # enforce boundaries
-  args.state.player_x = 0 if args.state.player_x < 0
-  args.state.player_y = 0 if args.state.player_y < 0
-
-  # rendering
-  args.outputs.sprites << [args.state.player_x, args.state.player_y, 100, 80, 'sprites/misc/dragon-0.png']
+  args.state.speed = diagonal?(args) ? 6 : 12
+  args.state.player_w = 100
+  args.state.player_h = 80
 end
 
 def diagonal?(args)
   (args.inputs.left_right) && (args.inputs.up_down)
+end
+
+def handle_input(args)
+  args.state.player_x += args.inputs.left_right * args.state.speed
+  args.state.player_y += args.inputs.up_down * args.state.speed
+end
+
+def enforce_boundaries(args)
+  if args.state.player_x + args.state.player_w > args.grid.w
+    args.state.player_x = args.grid.w - args.state.player_w
+  end
+
+  if args.state.player_y + args.state.player_h > args.grid.h
+    args.state.player_y = args.grid.h - args.state.player_h
+  end
+
+  args.state.player_x = 0 if args.state.player_x < 0
+  args.state.player_y = 0 if args.state.player_y < 0
 end
 
 # class Sprite
