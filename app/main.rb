26cad7e8fdbc4100cc672918a46f60b7ea4ff0b4
thewmking <thewmking@gmail.com>
2023-04-14 11:24:28 -0400

    Enforce boundaries

--- app/main.rb
+++ app/main.rb
@@ -6,7 +6,7 @@ def tick args
   # init
   args.state.player_x ||= 120
   args.state.player_y ||= 280
-  speed = 12
+  speed = diagonal?(args) ? 6 : 12
   player_w = 100
   player_h = 80
 
@@ -14,8 +14,24 @@ def tick args
   args.state.player_x += args.inputs.left_right * speed
   args.state.player_y += args.inputs.up_down * speed
 
+  if args.state.player_x + player_w > args.grid.w
+    args.state.player_x = args.grid.w - player_w
+  end
+
+  if args.state.player_y + player_h > args.grid.h
+    args.state.player_y = args.grid.h - player_h
+  end
+
+  # enforce boundaries
+  args.state.player_x = 0 if args.state.player_x < 0
+  args.state.player_y = 0 if args.state.player_y < 0
+
   # rendering
   args.outputs.sprites << [args.state.player_x, args.state.player_y, 100, 80, 'sprites/misc/dragon-0.png']
+end
+
+def diagonal?(args)
+  (args.inputs.left_right) && (args.inputs.up_down)
 end
 
 # class Sprite
