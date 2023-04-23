e89a9ff22d271894cdae2dbb0b86321c0844728e
thewmking <thewmking@gmail.com>
2023-04-22 22:30:37 -0400

    Display selected color in gameplay

--- app/lib/player_handler.rb
+++ app/lib/player_handler.rb
@@ -35,7 +35,7 @@ class PlayerHandler
     def animate_player(args)
       hold_for = InputHandler.player_is_moving?(args) ? 4 : 8
       sprite_index = 0.frame_index(count: 6, hold_for: hold_for, repeat: true)
-      args.state.player.path = "sprites/misc/dragon-#{sprite_index}.png"
+      args.state.player.path = "sprites/dragons/dragon-#{args.state.dragon.variant}-#{sprite_index}.png"
     end
 
     def enforce_boundaries(args)
